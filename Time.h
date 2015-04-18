6095ae46ccac123a06fb295ce8e7e377709d4ad2
RO169 <rowen.rungiah@gmail.com>
2015-04-18 14:05:06 +0200

    addedTime.h

---
+++ Time.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class Time
+{
+private:
+	int hours;
+	int minutes;
+
+public:
+	Time();
+	Time(int, int);
+	~Time();
+	void set(int, int);
+	void get(int &, int &);
+	friend ostream &operator << (ostream &, const Time &);
+	Time &operator ++();
+	Time operator++(int);
+
+};
+
